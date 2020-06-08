28d48c0ba8d428772451b8bfd07b0cd26ebc0501
Ulyses Rico <ulyses@quart.com.mx>
2020-06-08 12:04:22 -0500

    ...

--- jobServiceApp/main.py
+++ jobServiceApp/main.py
@@ -52,7 +52,7 @@ browser=webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chr
 
 def main():
     print('Main program of Quart...9th period ahead')
-    res=readUrl(2,0,2000000)
+    res=readUrl(2,0,1999955)
     print(res)  
     print("Main program is done")
     
@@ -281,7 +281,7 @@ def prepareThesis(id_thesis,json_thesis):
         thesis_html=''
         result=json_thesis
     else:
-        print('ID:'+strIdThesis)
+        print('Nope...ID:'+strIdThesis)
         result=''
         
     return  result
